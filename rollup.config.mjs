00df68f473f526e6cc8988b3e97c688394c27ed3
yuanzhang <945947732@qq.com>
2023-08-05 13:28:31 +0800

    style: 去除多余依赖

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,22 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import strip from "@rollup/plugin-strip";
 import typescript from "@rollup/plugin-typescript";
-import autoprefixer from "autoprefixer";
 import path from "path";
 import externals from "rollup-plugin-node-externals";
-import postcss from "rollup-plugin-postcss";
 
 import pkg from "./package.json" assert { type: "json" };
 
 export default [
   {
-    input: "./src/index.ts", // 入口文件
+    input: "./src/index.ts",
     output: [
       {
-        // 出口文件
         dir: path.dirname(pkg.module),
-        format: "es", // es模块导出，支持按需加载
+        format: "es",
         name: pkg.name,
-        exports: "named", // 指定导出模式（自动、默认、命名、无）
-        preserveModules: true, // 保留模块结构
-        preserveModulesRoot: "src", // 将保留的模块放在根级别的此路径下
+        exports: "named",
+        preserveModules: true,
+        preserveModulesRoot: "src",
       },
     ],
     plugins: [
@@ -38,12 +34,6 @@ export default [
         declaration: true,
         declarationDir: "es",
       }),
-      // 支持 scss，并添加前缀
-      postcss({
-        plugins: [autoprefixer()],
-      }),
-      // 清除调试代码
-      strip(),
     ],
   },
 ];
